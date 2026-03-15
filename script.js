51bc10912f814b350b533807d52273168397e163
Shintani Takahiro <ht22a049@oecu.jp>
2026-03-16 02:38:03 +0900

    リストに文字を追加する JS 機能を実装

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // ボタンを取得してクリック時に反応させる
 document.getElementById("add-btn").addEventListener("click", () => {
-	console.log("クリックされました");
+	// 「入力欄の文字を取得」するコード
+	const inputText = document.getElementById("input-text").value
+	const li = document.createElement("li"); // 新しい li 要素を作る
+	li.textContent = inputText; // 先ほど取得した文字をセット
+	document.getElementById("item-list").appendChild(li); // ul に追加
 });
+
