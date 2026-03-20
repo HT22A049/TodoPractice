10773b121703b790f7ce6a8e5b465b0fe954d060
Shintani Takahiro <ht22a049@oecu.jp>
2026-03-21 03:59:11 +0900

    タスク削除機能を追加し、HTML 構造と CSS を調整

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ document.getElementById("add-btn").addEventListener("click", () => {
 	// 「入力欄の文字を取得」するコード
 	const inputText = document.getElementById("input-text").value
 	const li = document.createElement("li"); // 新しい li 要素を作る
+	const del = document.createElement("button") // 削除ボタンの作成
+	del.textContent = "削除";
 	li.textContent = inputText; // 先ほど取得した文字をセット
+	del.addEventListener("click", () => {
+		li.remove();
+	});
+	li.appendChild(del);
 	document.getElementById("item-list").appendChild(li); // ul に追加
 	document.getElementById("input-text").value = ""; // 入力欄を空にする
 });
