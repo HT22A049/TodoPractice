86a44af1c338c48d2c2a15b727d02c64c9dc4a2a
Shintani Takahiro <ht22a049@oecu.jp>
2026-03-21 19:55:06 +0900

    タスクを Enter キーでも追加できる機能をを追加

--- script.js
+++ script.js
@@ -6,11 +6,31 @@ document.getElementById("add-btn").addEventListener("click", () => {
 	const del = document.createElement("button") // 削除ボタンの作成
 	del.textContent = "削除";
 	li.textContent = inputText; // 先ほど取得した文字をセット
+
 	del.addEventListener("click", () => {
 		li.remove();
 	});
+
 	li.appendChild(del);
 	document.getElementById("item-list").appendChild(li); // ul に追加
 	document.getElementById("input-text").value = ""; // 入力欄を空にする
 });
 
+const input = document.getElementById("input-text");
+input.addEventListener("keydown", (e) => {
+	if (e.key === "Enter") {
+		const inputText = document.getElementById("input-text").value
+		const li = document.createElement("li"); // 新しい li 要素を作る
+		const del = document.createElement("button") // 削除ボタンの作成
+		del.textContent = "削除";
+		li.textContent = inputText;
+
+		del.addEventListener("click", () => {
+			li.remove();
+		});
+
+		li.appendChild(del);
+		document.getElementById("item-list").appendChild(li); // ul に追加
+		document.getElementById("input-text").value = ""; // 入力欄を空にする
+	}
+})
